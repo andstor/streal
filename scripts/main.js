328cce800f9774eed49667496d533bf911e41353
André Storhaug <andr3.storhaug@gmail.com>
2018-03-19 21:22:50 +0100

    Fixes #29, hovering collor issue.

--- scripts/main.js
+++ scripts/main.js
@@ -9,12 +9,27 @@ function init() {
 
 
 function evtListeners() {
-    document.getElementById('hamburgerIcon').addEventListener('click', toggleDropdown);
+    document.getElementById('hamburgerIcon').addEventListener('click', toggleDropDown);
+
+    console.log("kkiifif");
+
+    document.getElementsByClassName('grid-menu')[0].querySelectorAll('li > a').forEach((e, i) => {
+        if (i === 0) {return}
+        e.addEventListener('mouseover', () => {
+            hoverNavbarTab(e, true);
+        }, false);
+
+        e.addEventListener('mouseleave', () => {
+            hoverNavbarTab(e, false);
+        }, false);
+
+
+    });
 }
 
 
-function toggleDropdown() {
-    var navbar = document.getElementById("navbar");
+function toggleDropDown() {
+    const navbar = document.getElementById("navbar");
 
     if (!navbar.classList.contains('responsive')) {
         navbar.className += " responsive";
@@ -23,10 +38,20 @@ function toggleDropdown() {
     }
 }
 
+function hoverNavbarTab(e, isHovered) {
+    if (isHovered === true) {
+        e.style.backgroundColor = "#62bfde";
+        document.getElementsByClassName('navbar-background')[0].style.borderColor = "#62bfde";
+    } else{
+        e.style.backgroundColor = "#fa4b89";
+        document.getElementsByClassName('navbar-background')[0].style.borderColor = "#fa4b89";
+    }
+
+
+}
 
 //Runs when the document is loaded. Initialises the script.
 document.addEventListener('DOMContentLoaded', function () {
     "use strict";
     init();
 });
-
