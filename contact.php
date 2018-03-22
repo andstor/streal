5ae52ceef1a96bfd9936b662fb9ec218ae1cca7f
André Storhaug <andr3.storhaug@gmail.com>
2018-03-23 00:13:15 +0100

    Contacts page finished.

--- contact.php
+++ contact.php
@@ -18,8 +18,9 @@
     <section class="grid-contact">
         <h1>Contact us</h1>
 
-        <img class="contactIMG" src="http://via.placeholder.com/400x260" alt="placeholder">
-
+        <iframe class="GoogleDirectionMap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d775.3256454650013!2d6.236603979850559!3d62.47162423509786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4616dac1b03a4a8b%3A0x5df22844dd93ce98!2sNTNU+i+%C3%85lesund!5e0!3m2!1sno!2sno!4v1521657351675"
+                width="400" height="260" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <div class="contact-caption">Anything you want to contact us about? Or maybe it's something we can do better? We always want to improve. Just write a message and send it to us.</div>
         <div class="col-3-4">
             <form class="" action="mailto:andstorh@stud.ntnu.no?subject=Feedback" method="post"
                   enctype="text/plain">
