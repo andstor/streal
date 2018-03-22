76cbc31ec2006d1267ef9064531e14e4d91186c9
Unknown <aronmar@live.no>
2018-03-22 21:38:51 +0100

    Removed "Home" link and added some text
    small changes

--- includes/footer.php
+++ includes/footer.php
@@ -2,20 +2,18 @@
     <div class="wrapper">
         <div class="footer-content">
             <h5>Streal</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab debitis dicta explicabo fugit illum
-                impedit
-                laboriosam neque odit provident quae quasi.
-                content.</p>
+            <p>
+                Streal is a revolutionary website where we dont do any research ourselves, but merely base it on others opinion.
+                Its a website that serve as a platform for reccomendations.
+             </p>
         </div>
 
         <div class="footer-links">
             <h5>Links</h5>
             <ul>
-                <li><a href="/index.php">Home</a></li>
                 <li><a href="/index.php">Blog</a></li>
                 <li><a href="/about.php">About</a></li>
                 <li><a href="/contact.php">Contact us</a></li>
-
                 <li><a href="/publish.php">Admin</a></li>
             </ul>
         </div>
