c2ab1ba768f73647c65cdfc77c620027e0283730
André Storhaug <andr3.storhaug@gmail.com>
2018-03-22 23:09:49 +0100

    Corrected link for about pictures, and some minor styling.

---
+++ about.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Streal</title>
+    <link rel="stylesheet" href="styles/main.css">
+    <link rel="stylesheet" href="styles/navbar.css">
+    <link rel="stylesheet" href="styles/contactabout.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script type="text/javascript" src="scripts/main.js"></script>
+</head>
+<body>
+
+<?php include_once 'includes/navbar.php' ?>
+
+
+<main class="wrapper">
+
+    <section class="aboutArticle grid-about">
+        <article class="grid-aboutText">
+            <h1 class="aboutIntroHeader">About Streal</h1>
+            <p>
+                Streal is a revolutionary website where we dont do any research ourselves,
+                but merely base it on others opinion. Its a website that serve as a platform for reccomendations,
+                akin to Metacritic and Rotten Tomatoes, but for gadgets and electronic devices.
+                We strive to find the best content and plagiarize it in the least hidden way.
+                This is the Streal culture.
+            </p>
+        </article>
+        <img class="aboutimg" src="http://via.placeholder.com/500x300" alt="placeholder">
+    </section>
+
+    <h1 class="aboutArticleHeader">The Team</h1>
+
+    <section class="aboutSection">
+        <!-- About members -->
+        <!-- Row 1 -->
+        <article class="contactBox rowSpacing">
+            <img class="contactProfileImage" src="images/contact_photos/andre.jpg" alt="selfieee">
+            <h3 class="contactProfileName">André Storhaug</h3>
+            <p class="rolletekst">Prosjektleder & Webutvikler</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="contentText">
+                12 years of IT competance.
+                Scrummplus founder and creator.
+                Makes the world's best pasta.
+            </p>
+        </article>
+        <article class="contactBox rowSpacing">
+            <img class="contactProfileImage" src="images/contact_photos/liban.jpg" alt="selfieee">
+            <h3 class="contactProfileName">Liban Nor</h3>
+            <p class="rolletekst">Webdesigner & Databaseadministrator</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="contentText">
+                12 years of IT competance.
+                Scrummplus founder and creator.
+                Makes the world's best pasta.
+            </p>
+        </article>
+        <article class="contactBox rowSpacing CBLast">
+            <img class="contactProfileImage" src="images/contact_photos/christian.jpg" alt="selfieee">
+            <h3 class="contactProfileName">Christian Leira</h3>
+            <p class="rolletekst">Fotoansvarlig & Webutvikler</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="contentText">
+                12 years of IT competance.
+                Scrummplus founder and creator.
+                Makes the world's best pasta.
+            </p>
+        </article>
+        <!-- Row 2 -->
+        <article class="contactBox rowSpacing CBStart">
+            <img class="contactProfileImage" src="images/contact_photos/kristin.jpg" alt="selfieee">
+            <h3 class="contactProfileName">Kristin Hagen</h3>
+            <p class="rolletekst">Fotograf & Skribent</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="contentText">
+                12 years of IT competance.
+                Scrummplus founder and creator.
+                Makes the world's best pasta.
+            </p>
+        </article>
+        <article class="contactBox rowSpacing">
+            <img class="contactProfileImage" src="images/contact_photos/aron.jpg" alt="selfieee">
+            <h3 class="contactProfileName">Aron M. Nicholasson</h3>
+            <p class="rolletekst">Informasjonsarkitekt, GUI Designer & Webutvikler</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: aronmn@stud.ntnu.no</p></i>
+            <p class="contentText">
+                DIY enthusiast, as well as a hobby musician.
+                All around nice guy, but can be toxic AF!
+            </p>
+        </article>
+        <article class="contactBox rowSpacing CBLast">
+            <img class="contactProfileImage" src="images/contact_photos/bendik.jpg" alt="selfieee">
+            <h3 class="contactProfileName">Bendik Nogva</h3>
+            <p class="rolletekst">Skribent & Webutvikler</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="contentText">
+                12 years of IT competance.
+                Scrummplus founder and creator.
+                Makes the world's best pasta.
+            </p>
+        </article>
+        <!-- Row 3 -->
+        <article class="contactBox rowSpacing">
+            <img class="contactProfileImage" src="images/contact_photos/marius.jpg" alt="selfieee">
+            <h3 class="contactProfileName">Marius B. Nakstad</h3>
+            <p class="rolletekst">Skribent & Grafisk Designer</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="contentText">
+                12 years of IT competance.
+                Scrummplus founder and creator.
+                Makes the world's best pasta.
+            </p>
+        </article>
+        <article class="contactBox rowSpacing">
+            <img class="contactProfileImage" src="images/contact_photos/sander.jpg" alt="selfieee">
+            <h3 class="contactProfileName">Sander J. Skarmyr</h3>
+            <p class="rolletekst">Innholdsansvarlig & Redaktør</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="contentText">
+                12 years of IT competance.
+                Scrummplus founder and creator.
+                Makes the world's best pasta.
+            </p>
+        </article>
+        <article class="contactBox rowSpacing CBLast">
+            <img class="contactProfileImage" src="images/contact_photos/vebjørn.jpg" alt="selfieee">
+            <h3 class="contactProfileName">Vebjørn Tomren</h3>
+            <p class="rolletekst">Skribent & Webutvikler</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="contentText">
+                12 years of IT competance.
+                Scrummplus founder and creator.
+                Makes the world's best pasta.
+            </p>
+        </article>
+
+    </section>
+
+</main>
+<?php include_once 'includes/footer.php' ?>
+
+</body>
+</html>
