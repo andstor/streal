8adba2215320f94110e22f3015342209a7e8c594
Unknown <aronmar@live.no>
2018-03-23 15:08:31 +0100

    Added text, img and set image to max-width
    read title :) :)

--- about.php
+++ about.php
@@ -28,7 +28,7 @@
                 This is the Streal culture.
             </p>
         </article>
-        <img class="aboutimg" src="http://via.placeholder.com/500x300" alt="placeholder">
+        <img class="aboutimg" src="images/contact_photos/strealitCropped.jpg" alt="placeholder">
     </section>
 
     <h1 class="aboutArticleHeader">The Team</h1>
@@ -39,52 +39,48 @@
         <article class="contactBox rowSpacing">
             <img class="contactProfileImage" src="images/contact_photos/andre.jpg" alt="selfieee">
             <h3 class="contactProfileName">André Storhaug</h3>
-            <p class="rolletekst">Prosjektleder & Webutvikler</p>
-            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="rolletekst">Project Manager & Webdeveloper</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: andstorh@stud.ntnu.no</p></i>
             <p class="contentText">
-                12 years of IT competance.
-                Scrummplus founder and creator.
-                Makes the world's best pasta.
+                Computer Engineering student with a severe case of talking.
+                Founder of Streal and Work-a-holic extraordinary.
             </p>
         </article>
         <article class="contactBox rowSpacing">
             <img class="contactProfileImage" src="images/contact_photos/liban.jpg" alt="selfieee">
             <h3 class="contactProfileName">Liban Nor</h3>
-            <p class="rolletekst">Webdesigner & Databaseadministrator</p>
-            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="rolletekst">Webdeveloper & Database Administrator</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: libanbn@stud.ntnu.no</p></i>
             <p class="contentText">
-                12 years of IT competance.
-                Scrummplus founder and creator.
-                Makes the world's best pasta.
+                Jesus F. Christ himself, PHP magician and modeling expert.
+                You got some sins to confess, this guy is your man!
             </p>
         </article>
         <article class="contactBox rowSpacing CBLast">
             <img class="contactProfileImage" src="images/contact_photos/christian.jpg" alt="selfieee">
             <h3 class="contactProfileName">Christian Leira</h3>
-            <p class="rolletekst">Fotoansvarlig & Webutvikler</p>
-            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="rolletekst">Photo Responsible & Webdeveloper</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: Chrilei@stud.ntnu.no</p></i>
             <p class="contentText">
-                12 years of IT competance.
-                Scrummplus founder and creator.
-                Makes the world's best pasta.
+                Computer Engineering student with decent experience
+                when it comes to working on websites.
+                He is also very talented when it comes to cooking hot dogs.
             </p>
         </article>
         <!-- Row 2 -->
         <article class="contactBox rowSpacing CBStart">
             <img class="contactProfileImage" src="images/contact_photos/kristin.jpg" alt="selfieee">
             <h3 class="contactProfileName">Kristin Hagen</h3>
-            <p class="rolletekst">Fotograf & Skribent</p>
-            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="rolletekst">Photographer & Writer</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: krisshag@stud.ntnu.no</p></i>
             <p class="contentText">
-                12 years of IT competance.
-                Scrummplus founder and creator.
-                Makes the world's best pasta.
+                The beauty among the beasts.
             </p>
         </article>
         <article class="contactBox rowSpacing">
             <img class="contactProfileImage" src="images/contact_photos/aron.jpg" alt="selfieee">
             <h3 class="contactProfileName">Aron M. Nicholasson</h3>
-            <p class="rolletekst">Informasjonsarkitekt, GUI Designer & Webutvikler</p>
+            <p class="rolletekst">Information Architect, GUI Designer & Webdeveloper</p>
             <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: aronmn@stud.ntnu.no</p></i>
             <p class="contentText">
                 DIY enthusiast, as well as a hobby musician.
@@ -94,46 +90,41 @@
         <article class="contactBox rowSpacing CBLast">
             <img class="contactProfileImage" src="images/contact_photos/bendik.jpg" alt="selfieee">
             <h3 class="contactProfileName">Bendik Nogva</h3>
-            <p class="rolletekst">Skribent & Webutvikler</p>
-            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="rolletekst">Writer & Webdeveloper</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: bendikun@stud.ntnu.no</p></i>
             <p class="contentText">
-                12 years of IT competance.
-                Scrummplus founder and creator.
-                Makes the world's best pasta.
+                Exceptionally well taught computer engineer,
+                extraordinary effective with Arduinos.
             </p>
         </article>
         <!-- Row 3 -->
         <article class="contactBox rowSpacing">
             <img class="contactProfileImage" src="images/contact_photos/marius.jpg" alt="selfieee">
             <h3 class="contactProfileName">Marius B. Nakstad</h3>
-            <p class="rolletekst">Skribent & Grafisk Designer</p>
-            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="rolletekst">Writer & Graphic Designer</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: mariubn@stud.ntnu.no</p></i>
             <p class="contentText">
-                12 years of IT competance.
-                Scrummplus founder and creator.
-                Makes the world's best pasta.
+                Doesn't know how it works, but knows how to get it to not work.
             </p>
         </article>
         <article class="contactBox rowSpacing">
             <img class="contactProfileImage" src="images/contact_photos/sander.jpg" alt="selfieee">
             <h3 class="contactProfileName">Sander J. Skarmyr</h3>
-            <p class="rolletekst">Innholdsansvarlig & Redaktør</p>
-            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="rolletekst">Content Manager & Editor</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: sanderjs@stud.ntnu.no</p></i>
             <p class="contentText">
-                12 years of IT competance.
-                Scrummplus founder and creator.
-                Makes the world's best pasta.
+                Complete lack of technicall skills, Keyboard hobbyist and master climber.
+                Unfortunaltely he struggles to get down.
             </p>
         </article>
         <article class="contactBox rowSpacing CBLast">
             <img class="contactProfileImage" src="images/contact_photos/vebjørn.jpg" alt="selfieee">
             <h3 class="contactProfileName">Vebjørn Tomren</h3>
-            <p class="rolletekst">Skribent & Webutvikler</p>
-            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: example@stud.ntnu.no</p></i>
+            <p class="rolletekst">Writer & Webdeveloper</p>
+            <i style="font-size:16px" class="fa">&#xf2bb; <p style="float:right;">: vebjorto@stud.ntnu.no</p></i>
             <p class="contentText">
-                12 years of IT competance.
-                Scrummplus founder and creator.
-                Makes the world's best pasta.
+                Eccentric hermit who is really passionate about his consoles,
+                and prefers long walks on the beach.
             </p>
         </article>
 
