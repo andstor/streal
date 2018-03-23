4abf264b0b0520d3f652ee21ae674aab93e55cfc
Jesus F. Christ <libanbn@stud.ntnu.no>
2018-03-23 03:57:27 +0100

    Publish page salvation
    # Changes
     - Change cover image direct links to uploads
     - Add extra configuration
     - Fix problems with field checking
     - Less aggressive
    
    # NOTE
     - Directories in configuration must exist
     - The article table must contain title, caption, category_id, cover, text, published and author as columns.

--- publish.php
+++ publish.php
@@ -6,30 +6,29 @@ require 'helpers/helper.php';
 
 $db = new Database();
 
-
 // Saves the results for later when it's going to be used in the menu
 $categoryResults = $db->select("SELECT * FROM category");
 
 // Checks if an article is getting published
 if (isset($_POST['post'])) {
 
-    // Removes the published article link
+    // Removes the published article link if set
     if (isset($_SESSION['link'])) {
         unset($_SESSION['link']);
     }
 
     // Saves the super globals to easily readable variables
     $title = $_POST['title'];
-    $img = $_POST['img'];
     $caption = $_POST['caption'];
+    $author = $_POST['author'];
     $cat = $_POST['cat_id'];
     $text = $_POST['text'];
 
     // Checks if the required fields aren't empty and cleans user input
-    if (isset($title) && isset($img) && isset($cat) && isset($text)) {
+    if ($title !== "" && isset($cat) && $text !== "" && $_FILES['upload']['name'] !== "" && $author !== "") {
         $title = preg_replace('/[^A-Za-z0-9 æ\ø\å\Æ\Ø\Å\-\&]/', '', (substr($db->link->real_escape_string($title), 0, 64)));
-        $img = htmlspecialchars(substr($db->link->real_escape_string($img), 0, 255), ENT_NOQUOTES);
         $caption = htmlspecialchars(substr($db->link->real_escape_string($caption), 0, 255), ENT_NOQUOTES);
+        $author = substr($db->link->real_escape_string($author), 0, 255);
         $cat = substr($db->link->real_escape_string($cat), 0, 5);
         $text = htmlentities($db->link->real_escape_string($text));
 
@@ -40,40 +39,40 @@ if (isset($_POST['post'])) {
 
             // Checks if category id written by user exists (change)
             if ($result->num_rows == 1) {
-                $img_header = @get_headers($img, 1);
+                // Prepares and uploads picture
+                $fileUploaded = false;
+                $siteURL = $_SERVER['SERVER_NAME'] . '/'; //root directory
+                $fileTempLocation = $_FILES["upload"]["tmp_name"];
+                $fileExtention = strtolower(pathinfo($_FILES["upload"]["name"], PATHINFO_EXTENSION)); //file extention
+                $fileName = md5(time() . $_FILES["upload"]["name"]) . '.' . $fileExtention; // (almost) unique file name
+                $fileFullLocalLink = LOCAL_DIR . UPLOAD_DIR . $fileName;
+                $fileFullPublicLink = 'http://' . $siteURL . UPLOAD_DIR . $fileName;
+                move_uploaded_file($fileTempLocation, $fileFullLocalLink);
+
+                $fileUploaded = true;
+
+                $img = $fileFullPublicLink;
 
                 // Checks if image link provided by user exists
-                if (strpos($img_header[0], '404') === false) {
+                if ($fileUploaded) {
+                    $time = date('Y-m-d H:m:s');
+                    $sql = "INSERT INTO article (title, caption, category_id, cover, text, published, author) 
+							VALUES ('$title', '$caption', $cat, '$img', '$text', '$time', '$author')";
+                    $result = $db->link->query($sql);
 
-                    // Checks if referenced file is an image
-                    if (strpos(str_replace("/", " ", $img_header['Content-Type']), 'image') !== false) {
-                        $time = date('Y-m-d H:m:s');
-                        $sql = "INSERT INTO article (title, caption, category_id, cover, text, published) 
-								VALUES ('$title', '$caption', $cat, '$img', '$text', '$time')";
-                        $result = $db->link->query($sql);
+                    $sql = "SELECT id, title, published FROM article WHERE title = '$title' AND published = '$time' LIMIT 1";
+                    $result = $db->select($sql);
 
-                        // Checks if article is published
-                        //	if (isset($result)) {
-                        $sql = "SELECT id, title, published FROM article WHERE title = '$title' AND published = '$time' LIMIT 1";
-                        $result = $db->select($sql);
-
-                        // Confirms that aricle exists within the database
-                        if ($result->num_rows == 1) {
-                            $row = $result->fetch_assoc();
-                            $articleID = $row['id'];
-                            $_SESSION['link'] = '<a href=\'../article.php?post=' . $articleID . '\'>Link to article >>></a>';
-                        } else {
-                            alert('Something went wrong...');
-                        }
-                        /*	}
-                            else {
-                                alert('Couldn\'t post the article.');
-                            }*/
+                    // Confirms that aricle exists within the database
+                    if ($result->num_rows == 1) {
+                        $row = $result->fetch_assoc();
+                        $articleID = $row['id'];
+                        $_SESSION['link'] = '<a href=\'../article.php?post=' . $articleID . '\'>Link to article >>></a>';
                     } else {
-                        alert('The file isn\'t an image.');
+                        alert('Something went wrong...');
                     }
                 } else {
-                    alert('Image doesn\'t exist.');
+                    alert('Couldn\'t upload cover image.');
                 }
             } else {
                 alert('Category id doesn\'t exist.');
@@ -82,8 +81,13 @@ if (isset($_POST['post'])) {
             alert('Category id must be a number.');
         }
     } else {
-        alert('Fill out all the fields.');
+        alert('Fill out all the fields and upload an image.');
     }
+
+    if (isset($result)) {
+        $result->free_result();
+    }
+
     $db->link->close();
 }
 
@@ -108,28 +112,24 @@ if (isset($_POST['post'])) {
 <main class='wrapper'>
     <section class="grid-publish">
         <br>
-        <form action='' method='POST' class='publish'>
+        <form action='' method='POST' class='publish' enctype="multipart/form-data">
             <fieldset>
                 <legend>Create an article</legend>
                 <input type='text' name='title' placeholder='Title'><br>
-                <input type='text' name='img' placeholder='Ex: http://link/to/image.jpg'><br>
                 <input type='text' name='caption' placeholder='Image caption'><br>
+                <input type='text' name='author' placeholder='Author'><br>
                 <select name='cat_id'>
                     <?php
                     //Lists all categories from database into the dropdown menu
                     while ($row = $categoryResults->fetch_array(MYSQLI_ASSOC)) {
                         echo '<option value="' . $row['id'] . '">' . $row['name'] . '</option>';
-                    }
-                    ?>
-                </select>
-                <br><br>
-                <br>
+                    } $categoryResults->free_result(); ?>
+                </select><br><br>
+                <input type="file" name="upload"><br><br>
                 <label>Article text</label><br>
                 <textarea name='text'></textarea><br>
                 <input type='submit' value='Publish' name='post'>
-                <?php if (isset($_SESSION['link'])) {
-                    echo $_SESSION['link'];
-                } ?>
+                <?php if (isset($_SESSION['link'])) echo $_SESSION['link']; ?>
             </fieldset>
         </form>
 
